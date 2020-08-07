1e27b6ea5893917f0acbc4f3aa45a06201f5ee0c
nam.nh <huunam101@gmail.com>
2020-08-07 13:06:14 +0700

    First commit to git

---
+++ ScrollRefresh/ScrollRefresh/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ScrollRefresh
+//
+//  Created by admin on 8/7/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ ScrollRefresh/ScrollRefresh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollRefresh
+//
+//  Created by admin on 8/7/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
